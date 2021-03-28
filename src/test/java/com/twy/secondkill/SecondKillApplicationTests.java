2e46bb3887a0b2f579d2fcb73e8ad77f9fc20299
twy <whgp1994@163.com>
2021-03-29 00:34:19 +0800

    build: 项目初始化

---
+++ src/test/java/com/twy/secondkill/SecondKillApplicationTests.java
@@ -0,0 +1,13 @@
+package com.twy.secondkill;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecondKillApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
